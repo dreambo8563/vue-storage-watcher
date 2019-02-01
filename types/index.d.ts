de4b7a560919c0c24329203cfb84704d58786de5
dreambo8563 <dreambo8563@outlook.com>
2019-02-01 11:41:23 +0800

    feat(strong type / better on mehtod with immediate opt. 1.0 released):

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,3 +8,16 @@ declare const LsWatcher: {
   ): void;
 };
 export default LsWatcher;
+
+interface ILSWatcher {
+  on(key: string, fn: Function, immediate?: boolean): Symbol;
+  set(key: string, payload: any, expire?: number | null): void;
+  broadcast(key: string, payload?: any): void;
+  broadcastAll(payload?: any): void;
+  has(key: string): boolean;
+  clear(): void;
+  get(key: string, def?: any): any;
+  init(): void;
+  remove(key: string): void;
+  off(key: string, handler: Symbol): void;
+}
