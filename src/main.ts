2142e1c9badcbb42041d391047b017c22923b12f
dreambo8563 <dreambo8563@outlook.com>
2019-05-12 21:45:56 +0800

    fix(keys replace issue):

--- src/main.ts
+++ src/main.ts
@@ -128,7 +128,7 @@ class LSWatcher {
       if (regexp.test(k) === false) {
         continue;
       }
-      myKeys.push(k.replace(regexp, ""));
+      myKeys.push(k.replace(this.prefix, ""));
     }
     return myKeys;
   }
